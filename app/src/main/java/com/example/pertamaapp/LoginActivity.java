58eb7b5cb2ac478421ae179fa9f1735e3e243075
avell-123 <150067051+Avelinawong@users.noreply.github.com>
2025-06-11 18:25:07 +0700

    mmm

---
+++ app/src/main/java/com/example/pertamaapp/LoginActivity.java
@@ -0,0 +1,33 @@
+package com.example.pertamaapp;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText name,password;
+    Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        name=findViewById(R.id.name);
+        password=findViewById(R.id.password);
+        login=findViewById(R.id.buttonlog);
+    }
+}
---
+++ app/src/main/java/com/example/pertamaapp/ProfileActivity.java
@@ -0,0 +1,58 @@
+package com.example.pertamaapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    Button btnsubmit;
+    TextView view,result;
+    EditText edtnama,edtprodi,edtemail,edtnilai1,edtnilai2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_profile);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        btnsubmit=findViewById(R.id.btnSubmit);
+        edtnama=findViewById(R.id.edtName);
+        edtprodi=findViewById(R.id.edtProdi);
+        edtemail=findViewById(R.id.edtEmail);
+        edtnilai1=findViewById(R.id.edtNilai1);
+        edtnilai2=findViewById(R.id.edtNilai2);
+        result=findViewById(R.id.txtResult);
+
+        btnsubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nama =edtnama.getText().toString();
+                String prodi=edtprodi.getText().toString();
+                String email=edtemail.getText().toString();
+                String nilai1=edtnilai1.getText().toString();
+                String nilai2=edtnilai2.getText().toString();
+
+                result.setText(
+                        nama +"\n"+
+                        prodi +"\n"+
+                        email+"\n"+
+                        nilai1+"\n"+
+                        nilai2+"\n"
+                );
+            }
+        });
+    }
+}
